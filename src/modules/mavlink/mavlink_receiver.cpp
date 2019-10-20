06206d4bd2b561a2c30b94f9578a6dccf4c5ffd1
Jaeyoung-Lim <jaeyoung@auterion.com>
2019-10-20 22:39:32 +0200

    Fix bitmask

--- src/modules/mavlink/mavlink_receiver.cpp
+++ src/modules/mavlink/mavlink_receiver.cpp
@@ -814,7 +814,7 @@ MavlinkReceiver::handle_message_set_position_target_local_ned(mavlink_message_t 
 		bool is_land_sp = (bool)(set_position_target_local_ned.type_mask & 0x2000);
 		bool is_loiter_sp = (bool)(set_position_target_local_ned.type_mask & 0x3000);
 		bool is_idle_sp = (bool)(set_position_target_local_ned.type_mask & 0x4000);
-		bool is_soaring_sp = (bool)(set_position_target_local_ned.type_mask & 0x5000);
+		bool is_soaring_sp = (bool)(set_position_target_local_ned.type_mask & 0x8000);
 
 		offboard_control_mode.timestamp = hrt_absolute_time();
 		_offboard_control_mode_pub.publish(offboard_control_mode);
